22447b236ab7130c5b773d32b9c3e16831f836a3
Joshua Ross <important@joshmross.com>
2022-05-28 17:25:56 -0400

    Update jershdesktop.ps1

--- jershdesktop.ps1
+++ jershdesktop.ps1
@@ -5,11 +5,10 @@ Pause
 Write-Output Creating User Account
 Pause
 #Config 
-Set-Variable /p user= What is the Username?
-Set-Variable /p pwd= What is the Password?
+Set-Variable user= What is the Username?
+Set-Variable pwd= What is the Password?
 net user %user% %pwd% /add -and net user %user% /PasswordChg:Tes -and WMIC USERACCOUNT WHERE Name='%user%' SET PasswordExpires=FALSE
+Pause
 
 Write-Output Installing Applications. Please Be Patient. -and
 winget import --import-file "winstall-8030.json" -and
-net user admin /del
-logoff
