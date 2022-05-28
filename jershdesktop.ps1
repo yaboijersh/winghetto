2556451c9babd0bf6219a45999b968b148f7b567
Joshua Ross <important@joshmross.com>
2022-05-28 17:16:21 -0400

    Update jershdesktop.ps1
    Possible Final Version of script doing some QA in a windows 11 VM.

--- jershdesktop.ps1
+++ jershdesktop.ps1
@@ -1,7 +1,15 @@
-@echo This Script us Automation on new Windows installs 
-Start-Process Powershell -Verb runAs &&
+Start-Process Powershell -Verb runAs
+Write-Output This Script us Automation on new Windows installs 
+Pause
+ 
+Write-Output Creating User Account
+Pause
+#Config 
+Set-Variable /p user= What is the Username?
+Set-Variable /p pwd= What is the Password?
+net user %user% %pwd% /add -and net user %user% /PasswordChg:Tes -and WMIC USERACCOUNT WHERE Name='%user%' SET PasswordExpires=FALSE
 
-
-
-
-winget import --import-file "winstall-8030.json" 
+Write-Output Installing Applications. Please Be Patient. -and
+winget import --import-file "winstall-8030.json" -and
+net user admin /del
+logoff
