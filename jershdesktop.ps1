a610f11923ee1c53447ed21aaee0c928e3b2491c
Joshua Ross <important@joshmross.com>
2022-05-28 20:15:15 -0400

    Update jershdesktop.ps1
    Changed the formatting for Write-Output. So it shows on the same line and does not obstruct the script

--- jershdesktop.ps1
+++ jershdesktop.ps1
@@ -1,15 +1,18 @@
 $title = "WinJersh Install Sctipt"
 $host.ui.RawUI.WindowTitle = $title
 
-Write-Output WinJersh Install Sctipt is used to create new accounts and Installing programs on my personal computers!
+Write-Output "WinJersh Install Sctipt is used to create new accounts and Installing programs on my personal computers!"
 Pause
  
 #Creating Accounts Config
-Write-Output Creating User Account
+Write-Output "Creating User Account"
 Pause
 $Cred = Get-Credential -Message 'Enter username and password for new local account'
-New-LocalUser -Name $Cred.UserName -Password $Cred.Password -and 
+New-LocalUser -Name $Cred.UserName -Password $Cred.Password 
+
+-and 
 
 #Program Installs
-Write-Output Installing Applications. Please Be Patient. -and
+Write-Output "Installing Applications. Please Be Patient." -and
 winget import --import-file "winstall-8073.json" 
+
